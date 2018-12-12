e27e12a6deaecc6f49a2f5dce802a88f4cad9ef3
ptitbgdu80 <35494499+ptitbgdu80@users.noreply.github.com>
2018-12-12 20:30:10 +0100

    Add files via upload

---
+++ mainInverse.cc
@@ -0,0 +1,16 @@
+#include "ProblemeInverse.h"
+#include <chrono>
+
+using namespace Eigen;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    cout << "Pas de fichier d'entrée" << endl;
+    exit(1);
+  }
+
+  return 0;
+}
